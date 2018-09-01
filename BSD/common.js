0d918451f9ffaf439bb82184670076e0a7e8f5c2
rafa <rafa@rafa-Aspire-one-1-131>
2018-09-01 17:58:01 +0200

    Binary division made

--- BSD/common.js
+++ BSD/common.js
@@ -2,14 +2,55 @@
 var display = new ROT.Display({width:80, height:20});
 var container = display.getContainer();
 
+
 function drawMap(width,height,map){
 	for(var i = 0; i < width; i++){
 		for(var j = 0; j < height; j++){
 			foreground = ROT.Color.toRGB([255,255,255]);
 			background = ROT.Color.toRGB([0,0,0]);
 			colors = "%c{" + foreground + "}%b{" + background + "}";
-			display.drawText(j,i,colors + map[(i*width)+j]);
+			display.drawText(i,j,colors + map[i][j]);
 		}
 	}
 }
 
+function divideSpace(topLeftCornerX,topLeftCornerY,width,height,minimun,counter){
+
+	if(counter < 1){
+		return //Finished
+	}
+
+	var division;
+	
+	if(Math.random() > 0.5){
+		division = "horizontal";
+	}else{
+		division = "vertical";
+	}
+
+	if(division == "horizontal"){
+		var randomY = Math.floor(Math.random() * (height - minimun * 2) + minimun); 
+		for(var i = 0; i < width; i++){
+			map[topLeftCornerX + i][topLeftCornerY + randomY] = counter;
+		}
+		if(randomY > minimun * 2){ //Check if the new space can be divided again
+			divideSpace(topLeftCornerX,topLeftCornerY,width,randomY,minimun,counter - 1);
+		}
+
+		if(height - randomY > minimun * 2){
+			divideSpace(topLeftCornerX,topLeftCornerY + randomY,width,height - randomY,minimun,counter - 1);
+		}
+	}
+	if(division == "vertical"){
+		var randomX = Math.floor(Math.random() * (width - minimun * 2) + minimun); 
+		for(var i = 0; i < height; i++){
+			map[topLeftCornerX + randomX][topLeftCornerY + i] = counter;
+		}
+		if(randomX > minimun * 2){
+			divideSpace(topLeftCornerX,topLeftCornerY,randomX,height,minimun,counter - 1);
+		}
+		if(width - randomX > minimun * 2){ 
+			divideSpace(topLeftCornerX + randomX,topLeftCornerY,width - randomX,height,minimun,counter - 1);
+		}
+	}
+}
--- BSD/game.js
+++ BSD/game.js
@@ -1,4 +1,16 @@
+
+var map = [];
+
+for(var i = 0; i < 80; i++){
+	map[i] = [];
+	for(var j = 0; j < 20; j++){
+		map[i].push(".");
+	}
+}
+
 window.onload = function() {
 	document.body.appendChild(container);
+	divideSpace(0,0,80,20,4,4);
+	drawMap(80,20,map);
 }
 

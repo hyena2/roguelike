4469c54e325f7134ffa1c48b3811a4072508e134
rafa <rafa@rafa-Aspire-one-1-131>
2018-09-01 22:56:30 +0200

    Making rooms done, need connections

--- BSD/common.js
+++ BSD/common.js
@@ -1,5 +1,5 @@
 
-var display = new ROT.Display({width:80, height:20});
+var display = new ROT.Display({width:80, height:40});
 var container = display.getContainer();
 
 
@@ -16,41 +16,82 @@ function drawMap(width,height,map){
 
 function divideSpace(topLeftCornerX,topLeftCornerY,width,height,minimun,counter){
 
-	if(counter < 1){
-		return //Finished
-	}
-
 	var division;
+	var randomX;
+	var randomY;
 	
-	if(Math.random() > 0.5){
-		division = "horizontal";
-	}else{
-		division = "vertical";
-	}
+	if(counter > 0){
 
-	if(division == "horizontal"){
-		var randomY = Math.floor(Math.random() * (height - minimun * 2) + minimun); 
-		for(var i = 0; i < width; i++){
-			map[topLeftCornerX + i][topLeftCornerY + randomY] = counter;
-		}
-		if(randomY > minimun * 2){ //Check if the new space can be divided again
-			divideSpace(topLeftCornerX,topLeftCornerY,width,randomY,minimun,counter - 1);
+		console.log("dividing space " + counter);
+
+		if(Math.random() > 0.5){
+			division = "horizontal";
+		}else{
+			division = "vertical";
 		}
 
-		if(height - randomY > minimun * 2){
-			divideSpace(topLeftCornerX,topLeftCornerY + randomY,width,height - randomY,minimun,counter - 1);
+		if(division == "horizontal"){
+			randomY = Math.floor(Math.random() * (height - minimun * 2) + minimun); 
+			//For developing porpuoses,remove when done
+			for(var i = 0; i < width; i++){
+				map[topLeftCornerX + i][topLeftCornerY + randomY] = counter;
+			}
+			//Check if the new space can be divided again
+			if(randomY > minimun * 2){ 
+				divideSpace(topLeftCornerX,topLeftCornerY,width,randomY,minimun,counter - 1);
+			}else{
+				//If subdivision isnt possible we already must make a room
+				generateRoom(topLeftCornerX,topLeftCornerY,width,randomY,minimun); 
+			}
+			if(height - randomY > minimun * 2){
+				divideSpace(topLeftCornerX,topLeftCornerY + randomY,width,height - randomY,minimun,counter - 1);
+			}else{
+				generateRoom(topLeftCornerX,topLeftCornerY + randomY,width,height - randomY,minimun);
+			}
+		}
+		if(division == "vertical"){
+			randomX = Math.floor(Math.random() * (width - minimun * 2) + minimun); 
+			//For developing porpuoses,remove when done
+			for(var i = 0; i < height; i++){
+				map[topLeftCornerX + randomX][topLeftCornerY + i] = counter;
+			}
+			if(randomX > minimun * 2){
+				divideSpace(topLeftCornerX,topLeftCornerY,randomX,height,minimun,counter - 1);
+			}else{
+				generateRoom(topLeftCornerX,topLeftCornerY,randomX,height,minimun);
+			}
+			if(width - randomX > minimun * 2){ 
+				divideSpace(topLeftCornerX + randomX,topLeftCornerY,width - randomX,height,minimun,counter - 1);
+			}else{
+				generateRoom(topLeftCornerX + randomX,topLeftCornerY,width - randomX,height,minimun);
+			}
 		}
 	}
-	if(division == "vertical"){
-		var randomX = Math.floor(Math.random() * (width - minimun * 2) + minimun); 
-		for(var i = 0; i < height; i++){
-			map[topLeftCornerX + randomX][topLeftCornerY + i] = counter;
-		}
-		if(randomX > minimun * 2){
-			divideSpace(topLeftCornerX,topLeftCornerY,randomX,height,minimun,counter - 1);
-		}
-		if(width - randomX > minimun * 2){ 
-			divideSpace(topLeftCornerX + randomX,topLeftCornerY,width - randomX,height,minimun,counter - 1);
+
+	//If this is the last iteration, we must do a room
+	if(counter == 0){
+		generateRoom(topLeftCornerX,topLeftCornerY,width,height,minimun)
+	}
+}
+
+function generateRoom(topLeftCornerX,topLeftCornerY,width,height,minimun){
+
+	var room = {width: null, height: null, x : null, y: null};
+	room.width = Math.floor(Math.random() * (width - minimun)) + minimun;
+	room.height = Math.floor(Math.random() * (height - minimun)) + minimun;
+
+	room.x = Math.floor(Math.random() * width);
+	while(room.x + room.width > width){
+		room.x = Math.floor(Math.random() * width);
+	}
+	room.y = Math.floor(Math.random() * height);
+	while(room.y + room.height > height){
+		room.y = Math.floor(Math.random() * height);
+	}
+
+	for(var i = 0; i < room.width; i++){
+		for (var j = 0; j < room.height; j++){
+			map[topLeftCornerX + room.x + i][topLeftCornerY + room.y + j] = "#";
 		}
 	}
 }
--- BSD/game.js
+++ BSD/game.js
@@ -3,14 +3,14 @@ var map = [];
 
 for(var i = 0; i < 80; i++){
 	map[i] = [];
-	for(var j = 0; j < 20; j++){
-		map[i].push(".");
+	for(var j = 0; j < 40; j++){
+		map[i].push("");
 	}
 }
 
 window.onload = function() {
 	document.body.appendChild(container);
-	divideSpace(0,0,80,20,4,4);
-	drawMap(80,20,map);
+	divideSpace(0,0,80,40,4,7);
+	drawMap(80,40,map);
 }
 

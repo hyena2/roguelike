7b46554ca68cef7a324aa59110878a11b766d49c
qxx3300 <rafael.paez.bastida@everis.com>
2018-11-19 17:39:00 +0100

    added 2d map support in input.js

--- input.js
+++ input.js
@@ -1,14 +1,13 @@
 document.onkeyup = function(key){
 
 	text = ""; //Reset text at the begining of the turn
-	currentTile = player.x + (player.y*80);
 
 	if(key.key == "ArrowRight"){
 		if(player.choosenCommand != null){
-			player.choosenCommand(map_Info[currentTile + 1]);
+			player.choosenCommand(state.mapInfo[state.player.x + 1][state.player.y]);
 			updateMap();
 		}else{
-			if(state.tiles[state.map.map[currentTile + 1]].solid == false){
+			if(state.tiles[state.map.map[state.player.x + 1][state.player.y]].solid == false){
 				updatedPlayer = state.player;
 				updatedPlayer.x++;
 				state.set(['player'],[updatedPlayer]);
@@ -19,10 +18,10 @@ document.onkeyup = function(key){
 
 	if(key.key == "ArrowLeft"){
 		if(player.choosenCommand != null){
-			player.choosenCommand(map_Info[currentTile - 1]);
+			player.choosenCommand(state.mapInfo[state.player.x - 1][state.player.y]);
 			updateMap();
 		}else{
-			if(state.tiles[state.map.map[currentTile - 1]].solid == false){
+			if(state.tiles[state.map.map[state.player.x - 1][state.player.y]].solid == false){
 				updatedPlayer = state.player;
 				updatedPlayer.x--;
 				state.set(['player'],[updatedPlayer]);
@@ -33,10 +32,10 @@ document.onkeyup = function(key){
 
 	if(key.key == "ArrowDown"){
 		if(player.choosenCommand != null){
-			player.choosenCommand(map_Info[currentTile + state.height]);
+			player.choosenCommand(state.mapInfo[state.player.x][state.player.y + 1]);
 			updateMap();
 		}else{
-			if(state.tiles[state.map.map[currentTile + state.width]].solid == false){
+			if(state.tiles[state.map.map[state.player.x][state.player.y + 1]].solid == false){
 				updatedPlayer = state.player;
 				updatedPlayer.y++;
 				state.set(['player'],[updatedPlayer]);
@@ -47,10 +46,10 @@ document.onkeyup = function(key){
 
 	if(key.key == "ArrowUp"){
 		if(player.choosenCommand != null){
-			player.choosenCommand(map_Info[currentTile - state.height]);
+			player.choosenCommand(state.mapInfo[state.player.x][state.player.y - 1]);
 			updateMap();
 		}else{
-			if(state.tiles[state.map.map[currentTile - state.width]].solid == false){
+			if(state.tiles[state.map.map[state.player.x][state.player.y - 1]].solid == false){
 				updatedPlayer = state.player;
 				updatedPlayer.y--;
 				state.set(['player'],[updatedPlayer]);
@@ -72,4 +71,3 @@ document.onkeyup = function(key){
 
 	drawText(text); //Display resulting text
 }
-

d34e666ec7cdd4615e8b3377230a868bfd768dc9
root <root@rafa-Aspire-one-1-131>
2018-11-28 00:16:48 +0100

    implemented enemy movement

--- npcUtils.js
+++ npcUtils.js
@@ -3,25 +3,45 @@ npcUtils = {
 		var dice = Math.round(Math.random() * 4);
 		switch (dice) {
 			case 0:
-				var npcsInTargetPosition = state.npcs.filter(n => n.posX == npc.posX - 1).length > 0 ? true : false;
-				var playerInTargetPosition = state.player.x == npc.posX - 1;
-				if (npc.posX > 0 && !npcsInTargetPosition && !playerInTargetPosition) npc.posX--;
-				break;
+				if(npc.posX > 0){
+					var npcsInTargetPosition = state.mapNpcs.filter(n => n.posX == npc.posX - 1).length > 0 ? true : false;
+					var playerInTargetPosition = state.player.x == npc.posX - 1 && state.player.y == npc.posY;
+					var targetTileIsSolid = state.tiles[state.map.map[npc.posX-1][npc.posY]].solid;
+					if (npc.posX > 0 && !npcsInTargetPosition && !playerInTargetPosition && !targetTileIsSolid) npc.posX--;
+					break;
+				}else{
+					break;
+				}
 			case 1:
-				var npcsInTargetPosition = state.npcs.filter(n => n.posX == npc.posX + 1).length > 0 ? true : false;
-				var playerInTargetPosition = state.player.x == npc.posX + 1;
-				if (npc.posX < state.height) npc.posX++;
-				break;
+				if(npc.posX < state.height){
+					var npcsInTargetPosition = state.mapNpcs.filter(n => n.posX == npc.posX + 1).length > 0 ? true : false;
+					var playerInTargetPosition = state.player.x == npc.posX + 1 && state.player.y == npc.posY;
+					var targetTileIsSolid = state.tiles[state.map.map[npc.posX+1][npc.posY]].solid;
+					if (!npcsInTargetPosition && !playerInTargetPosition && !targetTileIsSolid) npc.posX++;
+					break;
+				}else{
+					break;
+				}
 			case 2:
-				var npcsInTargetPosition = state.npcs.filter(n => n.posY == npc.posY - 1).length > 0 ? true : false;
-				var playerInTargetPosition = state.player.y == npc.posY - 1;
-				if (npc.posY > 0) npc.posY--;
-				break;
+				if(npc.posY > 0){
+					var npcsInTargetPosition = state.mapNpcs.filter(n => n.posY == npc.posY - 1).length > 0 ? true : false;
+					var playerInTargetPosition = state.player.y == npc.posY - 1 && state.player.x == npc.posX;
+					var targetTileIsSolid = state.tiles[state.map.map[npc.posX][npc.posY-1]].solid;
+					if (!npcsInTargetPosition && !playerInTargetPosition && !targetTileIsSolid) npc.posY--;
+					break;
+				}else{
+					break;
+				}
 			case 3:
-				var npcsInTargetPosition = state.npcs.filter(n => n.posY == npc.posY + 1).length > 0 ? true : false;
-				var playerInTargetPosition = state.player.y == npc.posY + 1;
-				if (npc.posY < state.width) npc.posY++;
-				break;
+				if(npc.posY < state.width){
+					var npcsInTargetPosition = state.mapNpcs.filter(n => n.posY == npc.posY + 1).length > 0 ? true : false;
+					var playerInTargetPosition = state.player.y == npc.posY + 1 && state.player.x == npc.posX;
+					var targetTileIsSolid = state.tiles[state.map.map[npc.posX][npc.posY+1]].solid;
+					if (!npcsInTargetPosition && !playerInTargetPosition && !targetTileIsSolid) npc.posY++;
+					break;
+				}else{
+					break;
+				}
 		}
 	}
 }

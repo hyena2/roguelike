22c8fcefd2708010863b06824b05f4059ba17c85
qxx3300 <rafael.paez.bastida@everis.com>
2018-12-07 09:42:07 +0100

    cleaned up files and started login

--- common.js
+++ common.js
@@ -9,106 +9,10 @@ function displayEntity(entity){
 	display.drawText(entity.x,entity.y,colors + entity.character);
 }
 
-function drawMap(width,height,map){
-	display.setOptions({
-		fontStyle: "bold",
-	});
-	display.clear();
-	for(var i = 0; i < width; i++){
-		for(var j = 0; j < height; j++){
-			foreground = ROT.Color.toRGB([255,255,255]);
-			background = ROT.Color.toRGB([0,0,0]);
-			colors = "%c{" + foreground + "}%b{" + background + "}";
-			display.drawText(i,j,colors + state.map.map[ i + (j * height)]);
-		}
-	}
-}
-
-function moveNpc(npc){
-
-	var directions = [0,1,2,3];
-	var dice = directions[Math.floor(Math.random() * directions.length)];
-	var currentTile = npc.x + (npc.y*height);
-	var moved = false;
-
-	if(dice == 0 && tiles[map_[currentTile + 1]].solid == false){
-		npc.x++;
-		moved = true;
-		map_Info[currentTile + 1].npc = npc; //Update map_Info
-		map_Info[currentTile].npc = null; //Reset old tile in map_Info
-	}
-
-	if(dice == 1 && tiles[map_[currentTile - 1]].solid == false){
-		npc.x--;
-		moved = true;
-		map_Info[currentTile - 1].npc = npc;
-		map_Info[currentTile].npc = null; 
-	}
-
-	if(dice == 2 && tiles[map_[currentTile + width]].solid == false){
-		npc.y++;
-		moved = true;
-		map_Info[currentTile + width].npc = npc;
-		map_Info[currentTile].npc = null; 
-	}
-
-	if(dice == 3 && tiles[map_[currentTile - width]].solid == false){
-		npc.y--;
-		moved = true;
-		map_Info[currentTile - width].npc = npc;
-		map_Info[currentTile].npc = null; 
-	}
-
-	//The function calls itself until it finds a tile free to move
-	if(!moved){
-		//Remove invalid direction
-		directions.splice[dice,1];
-		moved = moveNpc(npc,directions);
-	}
-}
-
-function attack(attacker,target){
-	if(target != null){
-		target.hp -= attacker.at - target.df;
-		if(target.hp < 0){
-			target.alive = false;
-			target.character = "x";
-		}
-	}
-}
 
 function drawText(text){
 	foreground = ROT.Color.toRGB([255,255,255]);
 	background = ROT.Color.toRGB([0,0,0]);
 	colors = "%c{" + foreground + "}%b{" + background + "}";
 	display.drawText(0,19,colors + text);
-}
-
-function updateMap(){
-	display.clear();
-	drawMap(80,20,map_);
-//	updateNpcs();
-	//displayItems();
-	player.display();
-}
-
-function updateNpcs(){
-	npcs.map(function(npc){
-		npc.think();
-		npc.display();
-	});
-}
-
-function displayItems(){
-	items.map(function(item){
-		if(item.x != null && item.y != null){ //Item with x or y null are in the inventory
-			item.display();
-		}
-	});
-}
-
-function take(taker,target){
-	taker.inventory.push(target);
-	target.x = null;
-	target.y = null;
 }
